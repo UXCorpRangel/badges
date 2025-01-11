64350e9ea2901a320db60d04e12f601c6777392e
Felix Icaza <fx.joliett17@gmail.com>
2025-01-10 22:38:20 -0600

    feat(svg): implement function and add default and rounded styles for primary badge

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,34 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { error as logger } from 'firebase-functions/logger'
+import { https } from 'firebase-functions/v2'
+import { defaultStyle, roundedStyle } from './primary'
 
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+export const badge = https.onRequest(async (request, response) => {
+  const { query } = request
 
-import * as logger from 'firebase-functions/logger'
-import { onRequest } from 'firebase-functions/v2/https'
+  try {
+    let svgContent
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+    // Verificar que el método de la solicitud sea GET
+    if (request.method !== 'GET') {
+      response.status(405).send(`Método ${request.method} no permitido.`)
+      return
+    }
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    // Generar el contenido SVG
+    if (query.style === 'default') {
+      svgContent = defaultStyle(query)
+    }
+
+    if (query.style === 'rounded') {
+      svgContent = roundedStyle(query)
+    }
+
+    // Establecer el tipo de contenido como image/svg+xml
+    response.set('Content-Type', 'image/svg+xml')
+    response.status(200).send(svgContent)
+  } catch (error) {
+    // Registrar el error y devolver un mensaje genérico
+    logger('Error manejando la solicitud:', error)
+    response.status(500).send('Error de servidor.')
+  }
+})
